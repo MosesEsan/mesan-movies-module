c4055b3f50f635b9ea4470d7f8ac701c6c31a9ae
Moses Esan <esanm@rte.ie>
2019-09-09 17:32:18 +0100

    Switched to use react navigation stack

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {createStackNavigator} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
 import Discover from "./scenes/Discover"
 import Movies from "./scenes/Movies"
